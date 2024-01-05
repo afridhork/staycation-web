23fd3bfc955ae089735c465822e5154880fd3d69
Afridho R Kartawiria <afridhorkartawiria@gmail.com>
2024-01-05 11:31:59 +0700

    Update Hero.tsx

--- src/parts/Hero.tsx
+++ src/parts/Hero.tsx
@@ -108,7 +108,7 @@ const Hero =forwardRef(({hero, isLoading}: {hero?: hero, isLoading?: boolean}, r
             ): isLoading && (
                <div className="row">
                   <div className="col-lg-6 col-12">
-                     <Skeleton className="w-[200px]"/>
+                     <Skeleton className="w-[200px] h-[300px]"/>
                   </div>
                   <div className="col-lg-6 col-12">
                      <Skeleton className="w-[200px]"/>
